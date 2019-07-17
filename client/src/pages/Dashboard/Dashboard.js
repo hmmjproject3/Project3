9122c0aab4410faf1c817e1e00fb148955730953
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-07-16 18:56:22 -0700

    dashboard grid done

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Dashboard.css'
+import Grid from '@material-ui/core/Grid'
+import ProgressChart from '../../components/ProgressChart'
+import SquadGoals from '../../components/SqualGoals/SquadGoals';
+import BonusChores from '../../components/BonusChores/BonusChores';
 
 const Dashboard = _ => {
     return (
-        <h1>Hello World</h1>
+        <>
+            <Grid container id='dashContainer' spacing={3} style={{textAlign: 'center', width: 'auto', margin: '50px'}}>
+                <Grid item id='progressChart' xs={12} style={{marginBottom: '10px'}}>
+                    <ProgressChart />
+                </Grid>
+                <Grid item id='squadGoals' xs={12} md={6}>
+                    <SquadGoals />
+                </Grid>
+                <Grid item id='bonusChores' xs={12} md={6}>
+                    <BonusChores />
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
