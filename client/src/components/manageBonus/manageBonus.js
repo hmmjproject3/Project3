1bddca2ae2f60827be81119e4c52b888673e3225
Matthew Villalobos <matthew.villalobos2010@gmail.com>
2019-07-17 19:08:20 -0700

    clean code if went through

--- client/src/components/manageBonus/manageBonus.js
+++ client/src/components/manageBonus/manageBonus.js
@@ -1,88 +1,28 @@
-import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-​
-const StyledTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: '#153B69',
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-​
-const StyledTableRow = withStyles(theme => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.background.default,
-    },
-  },
-}))(TableRow);
-​
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-​
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-​
+import react from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto',
+    backgroundColor: '#153B69',
+    padding: theme.spacing(0, 0),
+    width: 'auto'
   },
-  table: {
-    minWidth: 700,
-  },
-  main: {
-    marginLeft: 50,
-    marginRight: 50,
-  }
-}));
-​
-export default function CustomizedTables() {
-  const classes = useStyles();
-​
+}))
+
+const manageBonus = _ => {
+  const classes = useStyles()
   return (
-    <div className={classes.main}>
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell><h2>Dessert (100g serving)</h2></StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+    <div>
+      <Paper className={classes.root}>
+        <Grid item id='manageBonushead' xs={12} style={{ height: '50px', color: 'white', textAlign: 'right' }}>
+          <h3 style={{ padding: '10px', paddingTop: '13px' }}>manageBonus</h3>
+        </Grid>
+        <Grid item id='manageBonusbody' xs={12} style={{ height: '200px', backgroundColor: 'white' }} ></Grid>
+      </Paper>
     </div>
-  );
+  )
 }
+
+export default manageBonus
