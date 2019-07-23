92afafa0f4407988bc7034dd05c466fd526d5d25
nhijen <nhijenniferle@gmail.com>
2019-07-22 21:04:19 -0700

    backing up my work

--- client/src/components/ChildChores/childChores.js
+++ client/src/components/ChildChores/childChores.js
@@ -4,14 +4,31 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Selection from '../Selection'
 import ChoresContext from '../../utils/ChoresContext'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import ArrowDropDown from '@material-ui/icons/ExpandMore';
+import Delete from '@material-ui/icons/Delete';
+import Edit from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+
+import IconButton from '@material-ui/core/IconButton';
+
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundColor: '#153B69',
-        padding: theme.spacing(0, 0),
-        width: 'auto'
-    },
-  }))
+  root: {
+    backgroundColor: '#153B69',
+    padding: theme.spacing(0, 0),
+    width: 'auto'
+  },
+  table: {
+    width: '100%',
+    overflowX: 'auto',
+    minWidth: 300,
+  },
+}))
 
 const ChildChores = _ => {
 
@@ -19,33 +36,85 @@ const ChildChores = _ => {
   const classes = useStyles()
 
   return (
+   
     <div>
-        <Paper className={classes.root}>
-            <Grid style={{height: '50px', color: 'white', textAlign: 'left'}}>
-                <h3 style={{padding: '10px', paddingTop: '13px'}}>{child.name}   <Selection /></h3>
-               <>
+    <Paper style={{marginTop: '10px'}} className={classes.root}>
+    <Grid container spacing={3} style={{height: '60px', color: 'white', fontFamily: 'roboto', fontSize: '25px', textAlign: 'left'}}>
+              <Grid item xs={6}>
+              <p style={{color: 'white', padding: 0, marginTop: 0, marginLeft: 12, fontFamily: 'roboto', fontSize: '25px', }}>
+              {child.name}
+              </p>
+              </Grid>
+
+              <Grid item xs={6}  style={{height: '50px', color: 'white', fontFamily: 'roboto', fontSize: '25px', textAlign: 'right', margin: 0}}>
+              <Selection />
+              </Grid>
+     </Grid>
+     <Grid item id='bonusChoresBody' xs={12} style={{height: '250px', backgroundColor: 'white'}}>
+            <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{color: '#153B69'}} align="left">Chores Title</TableCell>
+              <TableCell style={{color: '#153B69'}} align="left">Cheddar</TableCell>
+              <TableCell style={{color: '#153B69'}} align="left">Progress</TableCell>
+              <TableCell style={{color: '#153B69'}} align="left"></TableCell>
+              <TableCell style={{color: '#153B69'}} align="left"></TableCell>
+
+
+            </TableRow>
+            </TableHead>
+            <TableBody>
+
+     <>
                   {
                     child.chores ? 
                     child.chores.map(chore => {
                       console.log(chore)
                       return (
                       <>
-                      <h1>{chore.task}</h1>
-                      <h1>{chore.points}</h1>
+                      <TableCell style={{color: '#153B69', width: '20px', paddingRight: '10px'}}>{chore.task}
+                      </TableCell>
+                      <TableCell style={{color: '#153B69', width: '20px', paddingRight: '10px'}}>{chore.points}</TableCell>
+                      <TableCell style={{color: '#153B69', width: '20px', paddingRight: '10px'}}>
+                      <IconButton>
+                      <ArrowDropDown/>
+                      </IconButton>
+                      </TableCell>
+
+                      <TableCell style={{color: '#153B69', width: '20px', paddingRight: '10px'}}>
+                      <IconButton>
+                      <Delete/>
+                      </IconButton>
+                      </TableCell>
+
+                      <TableCell style={{color: '#153B69', width: '20px', paddingRight: '10px'}}>
+                      <IconButton>
+                      <Edit/>
+                      </IconButton>
+                      </TableCell>
+
+
+
+                      {/* <h1 style={{ color: 'black', textAlign: 'left'}}>{chore.task}</h1>
+                      <h1 style={{ color: 'black', textAlign: 'left'}}>{chore.points}</h1> */}
                       </>
                     )
                   }) : null
                   }
 
                </>
-                </Grid>
-            <Grid xs={12} style={{height: '200px', backgroundColor: 'white'}} ></Grid>
-        </Paper>
-    </div>
+               </TableBody>
+          </Table>
+
+          <Button variant="contained" className={classes.button}>
+        Create New Chore
+      </Button>
+
+          </Grid>
+    </Paper>
+  </div> 
+
   )
 }
 
 export default ChildChores
-
-
-
