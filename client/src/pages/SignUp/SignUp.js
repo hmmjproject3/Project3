806ee7e449f25f1fa8fa4c09b62f4b72c270bbc7
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-13 20:58:45 -0700

    signin page for mobile

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -4,31 +4,29 @@ import { makeStyles } from '@material-ui/core/styles'
 import SignUpForm from '../../components/SignUpForm'
 import Grid from '@material-ui/core/Grid'
 
-const useStyles = makeStyles({
-  // mainCon: {
-  //   backgroundImage: `url(${Background})`,
-  //   backgroundPosition: 'center',
-  //   backgroundSize: 'cover',
-  //   backgroundRepeat: 'no-repeat',
-  //   height: 768
-  // },
+const useStyles = makeStyles(theme => ({
+  mainCon: {
+  },
   form: {
     boxShadow: 'none',
     paddingTop: 20,
     paddingBottom: 20,
     paddingLeft: 50,
-    paddingRight: 50
+    paddingRight: 50,
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+    }
   }
-})
+}))
 
 const SignUp = _ => {
   const classes = useStyles()
 
   return (
-    <div className='backgroundImg'>
-      <Grid container spacing={1}>
-        <Grid item xs={12} md={4} id='emptyLeft' />
-        <Grid item xs={12} md={8}>
+    <div id='backgroundImg'>
+      <Grid container spacing={1} className={classes.mainCon}>
+        <Grid item xs={12} md={4} className={classes.spongeGrid}/>
+        <Grid item xs={12} md={8} className={classes.formGrid}>
           <SignUpForm />
         </Grid>
       </Grid>
